ca5b906574fe97b0cfe650561c13589d50fc95bc
Bharath Raj M <32819433+BharathRajM@users.noreply.github.com>
2022-06-18 19:01:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ import utils
 
 import OurDDPG
 import SPG
+import TD3
 
 # In[17]:
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def eval_policy(policy, env_name,seed,eval_episodes=10):
@@ -38,7 +41,67 @@ def eval_policy(policy, env_name,seed,eval_episodes=10):
     print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
     print("---------------------------------------")
     return avg_reward
+    
+    
 
+def get_estimation_bias(policy_name,policy,env_name,discount,seed,max_steps,max_action):
+    
+    
+    eval_env = gym.make(env_name)
+    eval_env.seed(seed + 100)
+    
+    # get the starting state
+    state,done = eval_env.reset(),False
+    episode_reward = 0
+
+    if(policy_name=="SPG" or policy_name=="OurDDPG" or policy_name=="DDPG"):
+        #estimated Q(s,a)
+        starting_state = torch.FloatTensor(state.reshape(1,-1)).to(device)
+        starting_action = policy.actor(starting_state)
+        estimated_Q = policy.critic(starting_state,starting_action)
+
+        starting_action = starting_action.cpu().data.numpy().flatten()
+        estimated_Q = estimated_Q.cpu().data.numpy().flatten()
+
+        true_Q = 0
+
+        for t in range(int(max_steps)):
+
+            if(done):
+                break
+
+            action = (policy.select_action(np.array(state))).clip(-max_action,max_action)
+            next_state,reward,done,_ = eval_env.step(action)
+            state = next_state
+            episode_reward+=reward
+
+            true_Q=true_Q + (discount**t)*reward 
+    
+    if(policy_name=="TD3"):
+        #estimated Q(s,a)
+        starting_state = torch.FloatTensor(state.reshape(1,-1)).to(device)
+        starting_action = policy.actor(starting_state)
+        estimated_Q1,estimated_Q2 = policy.critic(starting_state,starting_action)
+        estimated_Q = torch.min(estimated_Q1,estimated_Q2)
+
+        starting_action = starting_action.cpu().data.numpy().flatten()
+        estimated_Q = estimated_Q.cpu().data.numpy().flatten()
+
+        true_Q = 0
+
+        for t in range(int(max_steps)):
+
+            if(done):
+                break
+
+            action = (policy.select_action(np.array(state))).clip(-max_action,max_action)
+            next_state,reward,done,_ = eval_env.step(action)
+            state = next_state
+            episode_reward+=reward
+
+            true_Q=true_Q + (discount**t)*reward
+            
+    return estimated_Q,true_Q
 
 if __name__ == "__main__":
     
@@ -69,6 +132,7 @@ if __name__ == "__main__":
     
     parser.add_argument("--save_model", action="store_true")        # Save model and optimizer parameters
     parser.add_argument("--load_model", default="")                 # Model load file name, "" doesn't load, "default" uses file_name
+    parser.add_argument("--get_estimates", default=1)                 # Model load file name, "" doesn't load, "default" uses file_name
     
     args = parser.parse_args()
     
@@ -84,6 +148,12 @@ if __name__ == "__main__":
 
     if True and not os.path.exists("./models"):
         os.makedirs("./models")
+        
+    if not os.path.exists("./results/"+args.env):
+        os.makedirs("./results/"+args.env)
+        
+    if not os.path.exists("./results/"+args.env+"/"+args.policy):
+        os.makedirs("./results/"+args.env+"/"+args.policy)
 
 
     env = gym.make(args.env)
@@ -143,13 +213,18 @@ if __name__ == "__main__":
 
     #evaluate untrained policy    
     evaluations = [eval_policy(policy, args.env, args.seed)]
+    #true Q value and estimated Q values
+    #true Q value and estimated Q values
+    true_Q = []
+    estimated_Q = []
 
     state,done = env.reset(),False
     episode_reward = 0
     episode_timesteps = 0
     episode_num = 0
-
-
+    
+    #keep track of rewards accumulated per timestep
+    training_rewards_list=[]
 
     for t in range(int(args.max_timesteps)):
 
@@ -171,7 +246,9 @@ if __name__ == "__main__":
 
         state = next_state
         episode_reward+=reward
-
+        
+        
+        
         if t>args.start_timesteps:
             
             if(args.policy == "SPG" or args.policy == "SPGR"):
@@ -184,21 +261,33 @@ if __name__ == "__main__":
             
 
         if done:
+            
             print(f"Total Timesteps: {t+1} Episode Num: {episode_num+1} Episode Timestep:{episode_timesteps} Reward:{episode_reward:.3f}")
-
+            training_rewards_list.append(episode_reward)
             state,done = env.reset(),False
             episode_reward = 0
             episode_timesteps = 0
             episode_num+=1
+            
+
 
         #Evaluate
         if (t+1) % args.eval_freq == 0:
             evaluations.append(eval_policy(policy,args.env,args.seed))
-            np.save(f"./results/{file_name}",evaluations)
+            np.save("./results/"+args.env+"/"+args.policy+"/"+file_name+"_eval_rewards",evaluations)
             if(True):
+                # save the model, rewards accumulated during training, rewards accumulated testing,
                 policy.save(f"./models/{file_name}")
-        
-
+                np.save("./results/"+args.env+"/"+args.policy+"/"+file_name+"_training_rewards",training_rewards_list)
+                #np.save(f"./results/{file_name}_training_rewards_list",training_rewards_list)
+                
+            if(args.get_estimates):
+                #Get Q value estimates
+                e_Q,t_Q = get_estimation_bias(args.policy,policy,args.env,args.discount,args.seed,args.max_timesteps,max_action)
+                estimated_Q.append(e_Q)
+                true_Q.append(t_Q)
+                np.save("./results/"+args.env+"/"+args.policy+"/"+file_name+"_true_Q",true_Q)
+                np.save("./results/"+args.env+"/"+args.policy+"/"+file_name+"_estimated_Q",estimated_Q)    
 
 
 
